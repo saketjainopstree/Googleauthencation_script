e1febefe640a520a701455bfca54a27d0af3a8b1
saket <saket.jain@opstree.com>
2018-01-26 15:47:23 +0000

    install file changes

--- installgooogleauthencation.sh
+++ installgooogleauthencation.sh
@@ -5,8 +5,8 @@ sudo apt-get install libpam-google-authenticator
 if [goole-authencation -eq 0 ]
 then
     echo "installing google authencation"
-    exit(0)
+    exit 0
 else
     echo "google authencation already installed"
-    exit(1)
+    exit 1
 fi
