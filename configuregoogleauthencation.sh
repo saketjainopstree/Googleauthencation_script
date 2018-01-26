7539782ee799d1090b73a9fcd5c350e1899d329f
saket <saket.jain@opstree.com>
2018-01-26 17:38:19 +0000

    changes in config

--- configuregoogleauthencation.sh
+++ configuregoogleauthencation.sh
@@ -4,7 +4,7 @@
 #RUN THIS SCRIPT NOW IT WILL CONFIGURE GOOGLE AUTHENCATION
 sudo echo 'auth required pam_google_authenticator.so' >> /etc/pam.d/sshd
 sudo echo 'AuthenticationMethods publickey,keyboard-interactive' >> /etc/ssh/sshd_config
-sudo sed -i '52s/@include common-auth/#@include common-auth/' /etc/pam.d/sshd
+sudo sed -i '4s/@include common-auth/#@include common-auth/' /etc/pam.d/sshd
 sudo sed -i '49s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i '52s/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
